b918f0903b8ea7c007cf64ba7ba134bf41176a14
Kirtiman Singh <kirt93@hotmail.com>
2018-07-16 16:49:30 +0100

    incomplete challenge #06: max balance- unsure :/

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,9 +2,11 @@ class Oystercard
   attr_reader :balance
   def initialize
     @balance = 0
+    
   end
 
   def top_up(amount)
+    # raise"limit reached" if (@balance += amount) > 90
     @balance += amount
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,11 +5,15 @@ describe Oystercard do
     it 'has a balance of zero' do
       expect(subject.balance).to eq(0)
     end
+
   end
   describe '#top-up' do
     it { is_expected.to respond_to(:top_up).with(1).argument}
     it "should top up the balance" do
-      expect{ subject.top_up(10) }.to change{ subject.balance }.by 10
+      expect{ subject.top_up(10) }.to change{ subject.balance }.by(10)
     end
+    # it "should not exceed balance limit" do
+    #   expect{ subject.top_up(91) }.to raise_error "limit reached"
+    # end
   end
 end
