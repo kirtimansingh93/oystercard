15067bc0b40c5df8d124ca8ef503c78f1a6ebf99
Kirtiman Singh <kirt93@hotmail.com>
2018-07-16 15:49:21 +0100

    complete challenge #05: top_up method to top up amount to balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,6 +1,15 @@
 require 'oystercard'
 describe Oystercard do
-  it 'has a balance of zero' do
-    expect(subject.balance).to eq(0)
+  describe '#balance' do
+    it { is_expected.to respond_to(:balance) }
+    it 'has a balance of zero' do
+      expect(subject.balance).to eq(0)
+    end
+  end
+  describe '#top-up' do
+    it { is_expected.to respond_to(:top_up).with(1).argument}
+    it "should top up the balance" do
+      expect{ subject.top_up(10) }.to change{ subject.balance }.by 10
+    end
   end
 end
